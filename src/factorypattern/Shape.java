ddc8d4b33ef1ed03c245f23f9cd2aacb7e99af5f
fahimer <ferdowsmohammadi@hotmail.com>
2018-05-20 20:25:35 +0200

    Educational  simple java program files for Super and Subclass

---
+++ src/factorypattern/Shape.java
@@ -0,0 +1,12 @@
+/*
+ * This is an interface class used demonstrate how factory pattern works
+ */
+package factorypattern;
+
+/**
+ *
+ * @author Elias
+ */
+public interface Shape {
+  void paint();  
+}
---
+++ src/factorypattern/Square.java
@@ -0,0 +1,16 @@
+package factorypattern;
+
+/**
+ *
+ * @author Elias
+ */
+public class Square implements Shape{
+
+    @Override
+    public void paint() {
+        System.out.println("Inside Square::draw() method.");
+    
+        
+    }
+    
+}
